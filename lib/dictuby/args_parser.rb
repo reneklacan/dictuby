fdb9de112242c18496c44fed1c92774b1d726183
Rene Klacan <rene.klacan@gmail.com>
2013-09-01 04:04:45 +0200

    -g option added

--- lib/dictuby/args_parser.rb
+++ lib/dictuby/args_parser.rb
@@ -2,12 +2,12 @@
 module Dictuby
     class ArgsParser
         def self.parse(args)
-
             options = {}
 
             OptionParser.new do |o|
                 o.on('-h')            { puts o; exit }
                 o.on('-l')            { |b| options[:list] = b }
+                o.on('-g')            { |b| options[:get] = b }
                 o.on('-s DICTIONARY') { |d| options[:set] = d }
                 o.on('QUERY')         { }
                 o.parse!
